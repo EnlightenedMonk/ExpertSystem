bc91bb51577260047b7e127e753f098e2ecf2834
EnlightenedMonk <filipiukrobert@gmail.com>
2016-10-23 15:30:09 +0200

    Images: Added Berserk, Mages (all 4) and Warrior Code: Disabled resizing of form's window and disabled minimize and maximize buttons

--- ExpertSystem/Form1.Designer.cs
+++ ExpertSystem/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnYes = new System.Windows.Forms.Button();
             this.btnNo = new System.Windows.Forms.Button();
             this.picResult = new System.Windows.Forms.PictureBox();
@@ -38,63 +39,50 @@
             // 
             // btnYes
             // 
-            this.btnYes.Location = new System.Drawing.Point(77, 344);
+            resources.ApplyResources(this.btnYes, "btnYes");
             this.btnYes.Name = "btnYes";
-            this.btnYes.Size = new System.Drawing.Size(75, 23);
-            this.btnYes.TabIndex = 0;
-            this.btnYes.Text = "TAK";
             this.btnYes.UseVisualStyleBackColor = true;
             this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
             // 
             // btnNo
             // 
-            this.btnNo.Location = new System.Drawing.Point(158, 344);
+            resources.ApplyResources(this.btnNo, "btnNo");
             this.btnNo.Name = "btnNo";
-            this.btnNo.Size = new System.Drawing.Size(75, 23);
-            this.btnNo.TabIndex = 1;
-            this.btnNo.Text = "NIE";
             this.btnNo.UseVisualStyleBackColor = true;
             this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
             // 
             // picResult
             // 
-            this.picResult.Location = new System.Drawing.Point(12, 12);
+            resources.ApplyResources(this.picResult, "picResult");
             this.picResult.Name = "picResult";
-            this.picResult.Size = new System.Drawing.Size(300, 300);
-            this.picResult.TabIndex = 3;
             this.picResult.TabStop = false;
             // 
             // btnRestart
             // 
-            this.btnRestart.Location = new System.Drawing.Point(274, 344);
+            resources.ApplyResources(this.btnRestart, "btnRestart");
             this.btnRestart.Name = "btnRestart";
-            this.btnRestart.Size = new System.Drawing.Size(37, 23);
-            this.btnRestart.TabIndex = 4;
-            this.btnRestart.Text = "R";
             this.btnRestart.UseVisualStyleBackColor = true;
             this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
             // 
             // txtQuestion
             // 
-            this.txtQuestion.Location = new System.Drawing.Point(12, 318);
+            resources.ApplyResources(this.txtQuestion, "txtQuestion");
             this.txtQuestion.Name = "txtQuestion";
-            this.txtQuestion.Size = new System.Drawing.Size(300, 20);
-            this.txtQuestion.TabIndex = 5;
-            this.txtQuestion.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(327, 380);
             this.Controls.Add(this.txtQuestion);
             this.Controls.Add(this.btnRestart);
             this.Controls.Add(this.picResult);
             this.Controls.Add(this.btnNo);
             this.Controls.Add(this.btnYes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.picResult)).EndInit();
             this.ResumeLayout(false);
